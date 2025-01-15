0cfc23c738d2482a9a73ddd2b1151bb4d0ab303c
TalElad <talab@elad.co.il>
2025-01-15 12:02:03 +0200

    making sql connection more stable

--- tools.py
+++ tools.py
@@ -42,7 +42,13 @@ connection = pymysql.connect(
 
 connection_string = f"mysql+pymysql://{username}:{password}@{endpoint}/{database}"
 
-engine = create_engine(connection_string)
+engine = create_engine(
+    connection_string,
+    pool_size=10,  # Number of persistent connections in the pool
+    max_overflow=5,  # Additional connections allowed beyond the pool
+    pool_recycle=1800,  # Recycle connections every 1800 seconds (30 minutes)
+    pool_pre_ping=True  # Validate connections before using
+)
 
 cursor = connection.cursor()
 cursor.execute("SELECT VERSION()")
